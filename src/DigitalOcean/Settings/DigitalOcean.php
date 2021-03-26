6dece3e2a6b38365b358a1551f737d8c91011e62
Pablo de la Peña <p@shedcollective.org>
2021-03-26 09:44:10 +0000

    Add getPermissions() to Settings

--- src/DigitalOcean/Settings/DigitalOcean.php
+++ src/DigitalOcean/Settings/DigitalOcean.php
@@ -38,6 +38,16 @@ class DigitalOcean implements Interfaces\Component\Settings
     /**
      * @inheritDoc
      */
+    public function getPermissions(): array
+    {
+        return [];
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * @inheritDoc
+     */
     public function get(): array
     {
         /** @var Setting $oAccessKey */
